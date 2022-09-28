7963426e872292a44df110a75421a8702d89dac6
gapizaver <gapi.levacic@gmail.com>
2022-09-28 21:43:38 +0200

    Day 13

---
+++ day13.py
@@ -0,0 +1,49 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    # Perform folding along y axis
+    def yfold(self, folding_line):
+        if self.y > folding_line:
+            self.y = 2*folding_line - self.y
+    
+    # Perform folding along x axis
+    def xfold(self, folding_line):
+        if self.x > folding_line:
+            self.x = 2*folding_line - self.x
+
+# Read input
+points = []
+with open("input.txt") as file:
+    for line in file:
+        if line.rstrip() != "":
+            if "," in line:
+                points.append(Point(
+                    (int) (line.rstrip().split(",")[0]),
+                    (int) (line.rstrip().split(",")[1])
+                ))
+            else:
+                # Perform folding
+                folding_line = (int) (line.rstrip().split()[2].split("=")[1])
+                axis = line.rstrip().split()[2].split("=")[0]
+                for point in points:
+                    # Folding along y axis
+                    if axis == "y":
+                        point.yfold(folding_line)
+
+                    # Folding along x axis
+                    else:
+                        point.xfold(folding_line)
+
+                # End after first fold
+                break 
+
+
+# Put points' coordinates in set
+visible_points_coordinates = set()
+for point in points:
+    visible_points_coordinates.add((point.x, point.y))
+
+# Print number of visible points - result
+print("Number of visible points after one fold:", len(visible_points_coordinates))
